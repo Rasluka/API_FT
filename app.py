7a8b791281859be64bd343eadb9f420c5a41ac8e
Jorge O <vargklee@hotmail.com>
2022-02-28 17:12:56 -0600

    Adding EDIT and UPDATE option in the list of Movies

--- app.py
+++ app.py
@@ -19,16 +19,18 @@ class Movie(db.Model):
         return f'Title: {self.title}, year: {self.year} and genre: {self.genre}'
 
 
-# Adding the routes to the templates
+# Route and function to the Index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Showing the list of movies
 @app.route('/movies/')
 def show_movies():
     movies = Movie.query.all()
     return render_template('movies.html', movies=movies)
 
+# Adding movies
 @app.route('/add-movie/', methods=['POST', 'GET'])
 def add_movie():
     if request.method == 'POST':
@@ -48,3 +50,35 @@ def add_movie():
 
     else:
         return render_template('add-movie.html')
+
+# Delete route and function
+@app.route('/delete/<int:id>')
+def delete(id):
+    movie_to_delete = Movie.query.get_or_404(id)
+
+    try:
+        db.session.delete(movie_to_delete)
+        db.session.commit()
+        return redirect(url_for('show_movies'))
+    except:
+        return 'We could not delete the movie!'
+
+# Update route and function
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+
+def update_movie(id):
+    movie_to_update = Movie.query.get_or_404(id)
+
+    if request.method == 'POST':
+        movie_to_update.title = request.form['title']
+        movie_to_update.year = request.form['year']
+        movie_to_update.director = request.form['director']
+        movie_to_update.genre = request.form['genre']
+
+        try:
+            db.session.commit()
+            return redirect(url_for('show_movies'))
+        except:
+            return 'We could not uptate the movie!'
+    else:
+        return render_template('update-movie.html', movie=movie_to_update)
---
+++ readme.md
@@ -0,0 +1,18 @@
+# The list of the movies that I have watched
+
+## This project was created using Flask, SQLite, HTML and some CSS (Havent added JS yet)
+
+In a effort to continue improving my dev skill I created this small project to put in practice everything I've recently learned about Flask, I had already some experience with Django therefore Flask wasnt that difficult to learn (at least the basics).
+
+What I tried to achive is to create a website that let me add a movie a its Title, Year, Director, Genre and planning to add my own Score. At the moment this project let me visualize the movies already added, add a new movie and update/delete an existing one.
+
+There are still plenty of things that I would be adding in the near future but of course the are more that I need to improve:
+* Add feedback (notification message) for the user whenever we create, update or delete a movie.
+* Improve the CSS code.
+* And some other small aspect.
+
+### Some images of the project
+
+![Alt text](img/demo-1.png?raw=true "Demo 1")
+![Alt text](img/demo-adding-movie.png?raw=true "Demo 2")
+![Alt text](img/demo-updating.png?raw=true "Demo 3")
